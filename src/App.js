3d6e98b5014d27f5a4213acb3611d1784b8842c0
noormohammad20 <mdsaim204@gmail.com>
2022-03-15 23:41:26 +0600

    jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+
+const number = 5555
+const singer = { name: 'dr mahfuz', job: 'singer' }
+const singer2 = { name: 'eva rahman', job: 'singer' }
+
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white'
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>hello dude react how are you</h3>
+        </div>
+        <div className="music">
+          <p>name:{2222 + number} </p>
+          <p style={singerStyle}>name:{singer.name} {singer.job}</p>
+          <p style={{ color: 'blue', backgroundColor: 'yellow' }}>name:{singer2.name} {singer2.job}</p>
+
+        </div>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +39,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
